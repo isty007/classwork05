21b80c0c856f19567e9f15178490f0ff18f5ca8c
ahmed imtiaz hossain <isty.khan1@gmail.com>
2018-07-18 04:33:33 +0000

    ClassWork_07

--- server.js
+++ server.js
@@ -1,18 +1,25 @@
-  var http = require('http');
-  var fs = require('fs');
-  
-  var server = http.createServer(function(req, res){
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/html');
-    fs.readFile('index.html',function(err,data){
-        if(err){
-            return console.log("file read error");
-        }
-    
-    res.end(data);
-  });
-  
-  });
+var http = require('http');
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+var server = http.Server(app);
+
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+});
+app.get('/about',function(req,res){
+  res.sendFile(__dirname+'/about.html');
+});
+app.get('/email',function(req,res){
+  res.sendFile(__dirname+'/email.html');
+});
+app.post('/submit_user',function(req,res)
+{
+    console.log(req.body);
+});
   server.listen(process.env.PORT, process.env.IP, function(){
     console.log('Server running');
   });
